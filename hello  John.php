f76fd954bb7085a864d7be4b86b20b4023b377c5
Emilio Ormeño <eormeno@gmail.com>
2022-08-12 20:51:53 -0300

    modifications

---
+++ hello  John.php
@@ -0,0 +1,13 @@
+<?php
+
+$boss = "John";
+$my_name = "Emilio";
+$age = 30;
+
+// Basic output with variables
+// $message1 = "Hello " . $boss . ", my name is " . $my_name . " and I am " . $age . " years old.";
+
+// Output with new line and inside variables
+// $mensaje2 = "Hello  $boss , my name is $my_name  and I am  $age  years old.\n";
+
+// echo $mensaje;
